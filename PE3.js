83547374b3ec9cfbcd3924df887cc80e5c437260
Briana Sullivan <briana.m.sullivan.18@gmail.com>
2021-09-05 20:20:51 -0700

    create loop that loops through function successfully

--- PE3.js
+++ PE3.js
@@ -1,18 +1,28 @@
-function test_prime(n) {
 
-    if (n === 1) {
-        return false;
-    }
-    else if (n === 2) {
-        return true;
-    } else {
-        for (var x = 2; x < n; x++) {
-            if (n % x === 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+let numbers = [];
+
+for (let b = 1; b < 20; b++) {
+    numbers.push(b);
 }
 
-console.log(test_prime(37));
+for (let i = 0; i < numbers.length; i++) {
+    function test_prime(n) {
+
+        if (n === 1) {
+            return false;
+        }
+        else if (n === 2) {
+            return true;
+        } else {
+            for (var x = 2; x < n; x++) {
+                if (n % x === 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    console.log(test_prime(numbers[i]));
+}
+
